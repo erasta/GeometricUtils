4bf6168c76a95b277fdbb0a987e6cf08fcecb824
erasta <eran.geva@gmail.com>
2017-05-24 20:09:10 +0300

    project point to mesh

---
+++ src/ProjectToMesh.js
@@ -0,0 +1,29 @@
+class ProjectToMesh {
+    constructor(geometry) {
+        this.geometry = geometry;
+    }
+
+    projectPoint(point) {
+        this.tri = this.tri || new THREE.Triangle();
+        this.candPoint = this.candPoint || new THREE.Vector3();
+        const verts = this.geometry.vertices;
+        let closest = new THREE.Vector3(),
+            closestFace, dist = 1e10;
+        for (let i = 0; i < this.geometry.faces.length; ++i) {
+            const f = this.geometry.faces[i];
+            this.tri.set(verts[f.a], verts[f.b], verts[f.c]);
+            this.tri.closestPointToPoint(point, this.candPoint);
+            const candDist = point.distanceTo(this.candPoint);
+            // console.log(Math.round(candDist*100)/100,this.candPoint.toArray(), '==', verts[f.a].toArray(), verts[f.b].toArray(), verts[f.c].toArray());
+            if (candDist < dist) {
+                // console.log('yes');
+                dist = candDist;
+                closest.copy(this.candPoint);
+                closestFace = i;
+            }
+        }
+        return { face: closestFace, point: closest, dist: dist };
+    }
+}
+
+if (typeof module !== 'undefined') module.exports.ProjectToMesh = ProjectToMesh;
--- test/tests.js
+++ test/tests.js
@@ -1,7 +1,14 @@
+function vec(x, y, z) { return new THREE.Vector3(x, y, z); }
 QUnit.test("closest point between segments", function(assert) {
-    function vec(x, y, z) { return new THREE.Vector3(x, y, z); }
     var closest = new SegmentsClosestPoints(vec(0, 0, 0), vec(10, 0, 0), vec(5, 5, 5), vec(5, -5, 5));
     assert.deepEqual(closest.point1, vec(5, 0, 0));
     assert.deepEqual(closest.point2, vec(5, 0, 5));
     assert.deepEqual(closest.dist, 5);
+});
+
+QUnit.test("project point to mesh", function(assert) {
+    var geom = new THREE.BoxGeometry(10, 10, 10);
+    var p = new ProjectToMesh(geom).projectPoint(vec(0, 1, 15));
+    assert.deepEqual(p.face, 8);
+    assert.deepEqual(p.point, vec(0, 1, 5));
 });
