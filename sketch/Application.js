e322711b8849a65cde64a17742654d5069ef30f0
erasta <eran.geva@gmail.com>
2017-05-27 20:31:53 +0300

    mesh projection few tests and sketch application

---
+++ sketch/Application.js
@@ -0,0 +1,45 @@
+class Application {
+    init() {
+        this.x1 = 3;
+        this.y1 = 4;
+        this.z1 = 15;
+        this.x2 = 2;
+        this.y2 = 4;
+        this.z2 = 15;
+
+        this.initGui();
+
+        var material = new THREE.MeshStandardMaterial({ color: 'red' });
+        this.mesh = new THREE.Mesh(new THREE.BoxGeometry(10, 10, 10), material);
+        this.sceneManager.scene.add(this.mesh);
+
+        this.applyGuiChanges();
+    }
+
+    applyGuiChanges() {
+        this.sceneManager.scene.remove(this.line);
+        this.line = new THREE.Line(new THREE.Geometry(), new THREE.LineBasicMaterial({color: 'blue'}));
+        const v1 = new THREE.Vector3(this.x1, this.y1, this.z1);
+        const v2 = new THREE.Vector3(this.x2, this.y2, this.z2);
+        this.line.geometry.vertices.push(v1, v2);
+        this.sceneManager.scene.add(this.line);
+
+        var actual = new ProjectToMesh(this.mesh.geometry).projectPolyline(this.line.geometry.vertices);
+        console.log('*****');actual.forEach(a => console.log(a.toArray()));
+        this.sceneManager.scene.remove(this.out);
+        this.out = new THREE.Line(new THREE.Geometry(), new THREE.LineBasicMaterial({color: 'green'}));
+        this.out.geometry.vertices = actual;
+        this.sceneManager.scene.add(this.out);
+    }
+
+    initGui() {
+        this.applyGuiChanges = this.applyGuiChanges.bind(this);
+        this.gui = new dat.GUI({ autoPlace: true, width: 500 });
+        this.gui.add(this, 'x1').min(-15).max(15).step(0.01).onChange(this.applyGuiChanges);
+        this.gui.add(this, 'y1').min(-15).max(15).step(0.01).onChange(this.applyGuiChanges);
+        this.gui.add(this, 'z1').min(-15).max(15).step(0.01).onChange(this.applyGuiChanges);
+        this.gui.add(this, 'x2').min(-15).max(15).step(0.01).onChange(this.applyGuiChanges);
+        this.gui.add(this, 'y2').min(-15).max(15).step(0.01).onChange(this.applyGuiChanges);
+        this.gui.add(this, 'z2').min(-15).max(15).step(0.01).onChange(this.applyGuiChanges);
+    }
+}
--- test/tests.js
+++ test/tests.js
@@ -1,4 +1,5 @@
 function vec(x, y, z) { return new THREE.Vector3(x, y, z); }
+
 QUnit.test("closest point between segments", function(assert) {
     var actual = new SegmentsClosestPoints(vec(0, 0, 0), vec(10, 0, 0), vec(5, 5, 5), vec(5, -5, 5));
     assert.deepEqual(actual.point1, vec(5, 0, 0));
@@ -14,10 +15,24 @@ QUnit.test("project point to mesh", function(assert) {
     assert.deepEqual(actual.dist, 10);
 });
 
+QUnit.test("project line to triangle", function(assert) {
+    var geom = new THREE.Geometry();
+    geom.vertices.push(vec(0, 0, 0), vec(10, 0, 0), vec(0, 10, 0))
+    geom.faces.push(new THREE.Face3(0, 1, 2));
+    var actual = new ProjectToMesh(geom).projectPolyline([vec(1, 1, 1), vec(2, 2, 2)]);
+    assert.deepEqual(actual, [vec(1, 1, 0), vec(2, 2, 0)]);
+});
+
+QUnit.test("project line to plane", function(assert) {
+    var geom = new THREE.PlaneGeometry(10, 10);
+    var actual = new ProjectToMesh(geom).projectPolyline([vec(-1, 1, 1), vec(-2, -4, 2)]);
+    assert.deepEqual(actual, [vec(-1, 1, 0), vec(-1.5, -1.5, 0), vec(-2, -4, 0)]);
+});
+
 QUnit.test("project line to mesh", function(assert) {
     var geom = new THREE.BoxGeometry(10, 10, 10);
-    var actual = new ProjectToMesh(geom).projectPolyline([vec(0, 15, 1), vec(0, 0, 15), vec(0, -15, 1)]);
-    console.log(actual);
+    var actual = new ProjectToMesh(geom).projectPolyline([vec(-3, -4, 15), vec(15, 3, 4)]);
+    actual.forEach(a => console.log(a.toArray()));
     // assert.deepEqual(p.face, 8);
     // assert.deepEqual(p.point, vec(0, 1, 5));
     // assert.deepEqual(p.dist, 10);
