75f4c69050ad49100c7e90dfbbfaae69b6f3d926
Eran Geva <eran@eran-mac.local>
2018-07-21 17:11:13 +0300

    change current point

--- app/js/AppProject.js
+++ app/js/AppProject.js
@@ -1,6 +1,10 @@
 class AppProject {
     init() {
         this.points = [new THREE.Vector3(2, 15, 5), new THREE.Vector3(2, -13, 15), new THREE.Vector3(15, 3, 1)];
+        this.currPoint = 2;
+        this.x = 0;
+        this.y = 0;
+        this.z = 0;
 
         this.initGui();
 
@@ -14,42 +18,52 @@ class AppProject {
     }
 
     applyGuiChanges() {
-        this.sceneManager.scene.remove(this.line);
-        this.sceneManager.scene.remove(this.out);
-        (this.spheres || []).forEach(s => this.sceneManager.scene.remove(s));
-
+        
         var actual = new ProjectToMesh(this.mesh.geometry).projectPolyline(this.points);
         
+        this.sceneManager.scene.remove(this.line);
         this.line = new THREE.Line(new THREE.Geometry(), new THREE.LineBasicMaterial({ color: 'blue' }));
         this.line.geometry.vertices = this.points;
         this.sceneManager.scene.add(this.line);
-
+        
+        this.sceneManager.scene.remove(this.out);
         this.out = new THREE.Line(new THREE.Geometry(), new THREE.LineBasicMaterial({ color: 'green' }));
         this.out.geometry.vertices = actual;
         this.sceneManager.scene.add(this.out);
+        
+        this.sceneManager.scene.remove(this.sphere);
+        this.sphere = new THREE.Mesh(new THREE.SphereGeometry(0.3), new THREE.MeshNormalMaterial());
+        this.sphere.position.copy(this.points[this.currPoint]);
+        this.sceneManager.scene.add(this.sphere);
 
-        this.spheres = this.points.map(p => {
-            var s = new THREE.Mesh(new THREE.SphereGeometry(0.3), new THREE.MeshNormalMaterial());
-            s.position.copy(p);
-            this.sceneManager.scene.add(s);
-        });
+        this.x = this.points[this.currPoint].x
+        this.y = this.points[this.currPoint].y
+        this.z = this.points[this.currPoint].z
         // console.log('*****');actual.forEach(a => console.log(a.toArray()));
     }
 
     newPoint() {
         var e = new THREE.Euler(Math.random() * 0.6 - 0.3, Math.random() * 0.6 - 0.3, Math.random() * 0.6 - 0.3);
         this.points.push(this.points[this.points.length - 1].clone().applyEuler(e));
+        this.currPoint = this.points.length - 1;
+        this.currPointCtrl.max(this.points.length - 1);
+        this.applyGuiChanges();
+    }
+
+    setCurrCoords() {
+        this.points[this.currPoint].set(this.x, this.y, this.z);
         this.applyGuiChanges();
     }
 
     initGui() {
         this.applyGuiChanges = this.applyGuiChanges.bind(this);
+        this.setCurrCoords = this.setCurrCoords.bind(this);
         this.gui = new dat.GUI({ autoPlace: true, width: 500 });
         this.gui.add(this, 'newPoint');
-        // this.gui.add(this, 'currPoint').min(0).max(2).step(1).onChange(this.applyGuiChanges);
-        // this.gui.add(this, 'y1').min(-15).max(15).step(0.01).onChange(this.applyGuiChanges);
-        // this.gui.add(this, 'z1').min(-15).max(15).step(0.01).onChange(this.applyGuiChanges);
-        // this.gui.add(this, 'x2').min(-15).max(15).step(0.01).onChange(this.applyGuiChanges);
+        this.currPointCtrl = this.gui.add(this, 'currPoint').min(0).max(2).step(1).onChange(this.applyGuiChanges);
+        this.gui.add(this, 'x').min(-15).max(15).step(0.01).onChange(this.setCurrCoords);
+        this.gui.add(this, 'y').min(-15).max(15).step(0.01).onChange(this.setCurrCoords);
+        this.gui.add(this, 'z').min(-15).max(15).step(0.01).onChange(this.setCurrCoords);
         // this.gui.add(this, 'y2').min(-15).max(15).step(0.01).onChange(this.applyGuiChanges);
         // this.gui.add(this, 'z2').min(-15).max(15).step(0.01).onChange(this.applyGuiChanges);
         // this.gui.add(this, 'x3').min(-15).max(15).step(0.01).onChange(this.applyGuiChanges);
