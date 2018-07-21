f3423c7baef55e28837831af23d65e80300ec98c
Eran Geva <eran@eran-mac.local>
2018-07-21 15:54:57 +0300

    adding random points

--- app/js/AppProject.js
+++ app/js/AppProject.js
@@ -1,14 +1,6 @@
 class AppProject {
     init() {
-        this.x1 = 2;
-        this.y1 = 15;
-        this.z1 = 5;
-        this.x2 = 2;
-        this.y2 = -13;
-        this.z2 = 15;
-        this.x3 = 15;
-        this.y3 = 3;
-        this.z3 = 1;
+        this.points = [new THREE.Vector3(2, 15, 5), new THREE.Vector3(2, -13, 15), new THREE.Vector3(15, 3, 1)];
 
         this.initGui();
 
@@ -17,42 +9,44 @@ class AppProject {
         // this.mesh = new THREE.Mesh(new THREE.BoxGeometry(10, 10, 10), new THREE.MeshStandardMaterial({ color: 'red', transparent: true, opacity: 0.3 }));
         this.sceneManager.scene.add(this.mesh);
         this.sceneManager.scene.add(new THREE.Mesh(this.mesh.geometry, new THREE.MeshBasicMaterial({ color: 'yellow', wireframe: true })));
-
+        
         this.applyGuiChanges();
     }
 
-    addLine(poly, color) {
-        const out = new THREE.Line(new THREE.Geometry(), new THREE.LineBasicMaterial({ color: color }));
-        out.geometry.vertices = poly;
-        this.sceneManager.scene.add(out);
-        return out;
+    applyGuiChanges() {
+        this.sceneManager.scene.remove(this.line);
+        this.sceneManager.scene.remove(this.out);
+
+        var actual = new ProjectToMesh(this.mesh.geometry).projectPolyline(this.points);
+        
+        this.line = new THREE.Line(new THREE.Geometry(), new THREE.LineBasicMaterial({ color: 'blue' }));
+        this.line.geometry.vertices = this.points;
+        this.sceneManager.scene.add(this.line);
+
+        this.out = new THREE.Line(new THREE.Geometry(), new THREE.LineBasicMaterial({ color: 'green' }));
+        this.out.geometry.vertices = actual;
+        this.sceneManager.scene.add(this.out);
+
+        // console.log('*****');actual.forEach(a => console.log(a.toArray()));
     }
 
-    applyGuiChanges() {
-        const v1 = new THREE.Vector3(this.x1, this.y1, this.z1);
-        const v2 = new THREE.Vector3(this.x2, this.y2, this.z2);
-        const v3 = new THREE.Vector3(this.x3, this.y3, this.z3);
-
-        this.sceneManager.scene.remove(this.line);
-        this.line = this.addLine([v1, v2, v3], 'blue');
-
-        var actual = new ProjectToMesh(this.mesh.geometry).projectPolyline([v1, v2, v3]);
-        this.sceneManager.scene.remove(this.out);
-        this.out = this.addLine(actual, 'green');
-        // console.log('*****');actual.forEach(a => console.log(a.toArray()));
+    newPoint() {
+        this.points.push(new THREE.Vector3(Math.random() * 30 - 15, Math.random() * 30 - 15, Math.random() * 30 - 15));
+        this.applyGuiChanges();
     }
 
     initGui() {
         this.applyGuiChanges = this.applyGuiChanges.bind(this);
         this.gui = new dat.GUI({ autoPlace: true, width: 500 });
-        this.gui.add(this, 'x1').min(-15).max(15).step(0.01).onChange(this.applyGuiChanges);
-        this.gui.add(this, 'y1').min(-15).max(15).step(0.01).onChange(this.applyGuiChanges);
-        this.gui.add(this, 'z1').min(-15).max(15).step(0.01).onChange(this.applyGuiChanges);
-        this.gui.add(this, 'x2').min(-15).max(15).step(0.01).onChange(this.applyGuiChanges);
-        this.gui.add(this, 'y2').min(-15).max(15).step(0.01).onChange(this.applyGuiChanges);
-        this.gui.add(this, 'z2').min(-15).max(15).step(0.01).onChange(this.applyGuiChanges);
-        this.gui.add(this, 'x3').min(-15).max(15).step(0.01).onChange(this.applyGuiChanges);
-        this.gui.add(this, 'y3').min(-15).max(15).step(0.01).onChange(this.applyGuiChanges);
-        this.gui.add(this, 'z3').min(-15).max(15).step(0.01).onChange(this.applyGuiChanges);
+        this.gui.add(this, 'newPoint');
+        // this.gui.add(this, 'x1').min(-15).max(15).step(0.01).onChange(this.applyGuiChanges);
+        // this.gui.add(this, 'y1').min(-15).max(15).step(0.01).onChange(this.applyGuiChanges);
+        // this.gui.add(this, 'z1').min(-15).max(15).step(0.01).onChange(this.applyGuiChanges);
+        // this.gui.add(this, 'x2').min(-15).max(15).step(0.01).onChange(this.applyGuiChanges);
+        // this.gui.add(this, 'y2').min(-15).max(15).step(0.01).onChange(this.applyGuiChanges);
+        // this.gui.add(this, 'z2').min(-15).max(15).step(0.01).onChange(this.applyGuiChanges);
+        // this.gui.add(this, 'x3').min(-15).max(15).step(0.01).onChange(this.applyGuiChanges);
+        // this.gui.add(this, 'y3').min(-15).max(15).step(0.01).onChange(this.applyGuiChanges);
+        // this.gui.add(this, 'z3').min(-15).max(15).step(0.01).onChange(this.applyGuiChanges);
     }
 }
