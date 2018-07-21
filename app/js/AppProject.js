a83003068c5a9d097fbc7d00a25222b65b4eafff
Eran Geva <eran@eran-mac.local>
2018-07-21 16:15:02 +0300

    spheres

--- app/js/AppProject.js
+++ app/js/AppProject.js
@@ -16,6 +16,7 @@ class AppProject {
     applyGuiChanges() {
         this.sceneManager.scene.remove(this.line);
         this.sceneManager.scene.remove(this.out);
+        (this.spheres || []).forEach(s => this.sceneManager.scene.remove(s));
 
         var actual = new ProjectToMesh(this.mesh.geometry).projectPolyline(this.points);
         
@@ -27,11 +28,17 @@ class AppProject {
         this.out.geometry.vertices = actual;
         this.sceneManager.scene.add(this.out);
 
+        this.spheres = this.points.map(p => {
+            var s = new THREE.Mesh(new THREE.SphereGeometry(0.3), new THREE.MeshNormalMaterial());
+            s.position.copy(p);
+            this.sceneManager.scene.add(s);
+        });
         // console.log('*****');actual.forEach(a => console.log(a.toArray()));
     }
 
     newPoint() {
-        this.points.push(new THREE.Vector3(Math.random() * 30 - 15, Math.random() * 30 - 15, Math.random() * 30 - 15));
+        var e = new THREE.Euler(Math.random() * 0.6 - 0.3, Math.random() * 0.6 - 0.3, Math.random() * 0.6 - 0.3);
+        this.points.push(this.points[this.points.length - 1].clone().applyEuler(e));
         this.applyGuiChanges();
     }
 
@@ -39,7 +46,7 @@ class AppProject {
         this.applyGuiChanges = this.applyGuiChanges.bind(this);
         this.gui = new dat.GUI({ autoPlace: true, width: 500 });
         this.gui.add(this, 'newPoint');
-        // this.gui.add(this, 'x1').min(-15).max(15).step(0.01).onChange(this.applyGuiChanges);
+        // this.gui.add(this, 'currPoint').min(0).max(2).step(1).onChange(this.applyGuiChanges);
         // this.gui.add(this, 'y1').min(-15).max(15).step(0.01).onChange(this.applyGuiChanges);
         // this.gui.add(this, 'z1').min(-15).max(15).step(0.01).onChange(this.applyGuiChanges);
         // this.gui.add(this, 'x2').min(-15).max(15).step(0.01).onChange(this.applyGuiChanges);
